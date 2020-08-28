2f6d564fb5b0daa7ae0c4e82bd0d6e7cb4e78bc9
marius david <mariusdavid@laposte.net>
2020-08-28 23:22:52 +0200

    added a feature to play all music syncronously

--- static/musicplayer.js
+++ static/musicplayer.js
@@ -1,28 +1,35 @@
 var music_players = document.getElementsByClassName("audiopreview");
 
+var can_play_multiple_music = false;
+var auto_play_next_music = true;
+
 for (player of music_players) {
 	player.addEventListener("mouseover", function(e) {
 		e.target.preload = "auto";
 	}); //TODO: maybe use the whole tile choose to preload
 	player.addEventListener("playing", function(e) {
-		var next_id = Number(e.target.getAttribute("audiopreview_nb")) + 1;
-		for (player of music_players) {
-			var player_id = player.getAttribute("audiopreview_nb");
-			if (player_id != e.target.getAttribute("audiopreview_nb")) {
-				player.pause();
-				player.fastSeek(0);
-			};
-			if (player_id == next_id) {
-				player.preload = "auto";
+		if (!can_play_multiple_music) {
+			var next_id = Number(e.target.getAttribute("audiopreview_nb")) + 1;
+			for (player of music_players) {
+				var player_id = player.getAttribute("audiopreview_nb");
+				if (player_id != e.target.getAttribute("audiopreview_nb")) {
+					player.pause();
+					player.fastSeek(0);
+				};
+				if (player_id == next_id) {
+					player.preload = "auto";
+				};
 			};
 		};
 	});
 	player.addEventListener("ended", function(e) {
 		e.target.fastSeek(0);
-		var next_id = Number(e.target.getAttribute("audiopreview_nb")) + 1;
-		for (player of music_players) {
-			if (player.getAttribute("audiopreview_nb") == String(next_id)) {
-				player.play();
+		if (auto_play_next_music) {
+			var next_id = Number(e.target.getAttribute("audiopreview_nb")) + 1;
+			for (player of music_players) {
+				if (player.getAttribute("audiopreview_nb") == String(next_id)) {
+					player.play();
+				};
 			};
 		};
 	});
@@ -32,3 +39,12 @@ for (player of music_players) {
 		};
 	})
 }
+
+document.getElementById("play_all").addEventListener("click", function() {
+	can_play_multiple_music = true;
+	auto_play_next_music = false;
+	for (player of music_players) {
+		player.fastSeek(0);
+		player.play();
+	};
+});
