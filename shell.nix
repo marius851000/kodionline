7a74c9c40ff5c7cf424776c5be24e2d01add7c90
marius david <mariusdavid@laposte.net>
2020-09-01 08:56:15 +0000

    include urldecode in the dev environment

--- shell.nix
+++ shell.nix
@@ -1,5 +1,20 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, naersk ? import (pkgs.fetchFromGitHub {
+	owner = "nmattia";
+	repo = "naersk";
+	rev = "529e910a3f423a8211f8739290014b754b2555b6";
+	sha256 = "3pDN/W17wjVDbrkgo60xQSb24+QAPQ7ulsUq5atNni0=";
+})}:
 
+let
+	naersk-lib = pkgs.callPackage naersk { };
+
+	urlencode = naersk-lib.buildPackage (pkgs.fetchFromGitHub {
+		owner = "dead10ck";
+		repo = "urlencode";
+		sha256 = "iiebonXsCLZDsUCoWZ9zazDR+lpNQHNrb+vYJ6M8qVA=";
+		rev = "0.1.2";
+	});
+in
 pkgs.stdenv.mkDerivation {
 	name = "kodionline";
 	propagatedBuildInputs = with pkgs.python2Packages; [
@@ -12,9 +27,12 @@ pkgs.stdenv.mkDerivation {
 	nativeBuildInputs = with pkgs; [
 		fish
 		python2
+		urlencode
 	];
+ 	
+	NIX_ENFORCE_PURITY=0;
 
-	preConfigure = ''
+	shellHook = ''
 		export PYTHONPATH=$PYTHONPATH:/home/marius/kodi-dl
 		fish
 		exit
